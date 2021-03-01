5e84211c54899956111f02ac730fe49df55979ff
dmitriy-re <my@my.com>
2021-03-01 18:24:25 +0300

    turn on nfs server

--- nfss_script.sh
+++ nfss_script.sh
@@ -4,6 +4,7 @@ mkdir /var/nfs_share
 mkdir /var/nfs_share/upload
 chmod 777 /var/nfs_share/upload/
 systemctl start nfs
+systemctl enable nfs-server
 echo "/var/nfs_share 192.168.50.11(ro)" > /etc/exports
 echo "/var/nfs_share/upload 192.168.50.11(rw)" >> /etc/exports
 exportfs -r
